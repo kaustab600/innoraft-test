66803849cac414a370a6c6bda1adba68808693fc
kaustab600 <kaustavroy1111@gmail.com>
2020-02-24 00:41:54 +0530

    removed id selectors from html

--- jquery-task/javascriptfunctions.js
+++ jquery-task/javascriptfunctions.js
@@ -1,12 +1,13 @@
-document.getElementById('btn1').onclick=function func(){
+document.querySelector('.section1 button').onclick=function func(){
 
-	document.getElementById('box1').style.width = "300px";
+	document.querySelector('.section1 .height').style.width = "300px";
+
 }
 
-document.getElementById('btn2').onclick=function func(){
+document.querySelector('.section2 button').onclick=function func(){
 
-	document.getElementById('box2').style.transform = "translateX(400px)";
-	document.getElementById('box2').style.transition = "transform 2s";
+	document.querySelector('.section2 .height').style.transform = "translateX(400px)";
+	document.querySelector('.section2 .height').style.transition = "transform 2s";
 }
 
 window.onscroll=function func(){
@@ -15,19 +16,19 @@ window.onscroll=function func(){
 	var a = document.documentElement.scrollTop;
 	if(a>700 || a<900)
 	{
-		document.getElementById('scrollsection').style.position = "sticky";
-		document.getElementById('scrollsection').style.top = "0px";
+		document.querySelector('.full').style.position = "sticky";
+		document.querySelector('.full').style.top = "0px";
 	}
 }
 
 
 
-document.getElementById('btn3').onclick=function func(){
-
+document.querySelector('.section5 button').onclick=function func(){
+				
 				// create wrapper container
 			var wrapper = document.createElement('div');
 
-			var wrapelement = document.getElementById('wrapelement')
+			var wrapelement = document.querySelector('.section5 p:nth-of-type(2)');
 				// insert wrapper before el in the DOM tree
 			wrapelement.parentNode.insertBefore(wrapper, wrapelement);
 
@@ -37,32 +38,32 @@ document.getElementById('btn3').onclick=function func(){
 }
 
 
-document.getElementById('btn4').onclick = function func(){
+document.querySelector('.section9 button').onclick = function func(){
 
-	document.getElementById('test3').disabled = true;
+	document.querySelector('.section9 input[type="text"][value="test value"]').disabled = true;
 }
 
 
-document.getElementById('btn5').onclick =  function func(){
+document.querySelector('.section6 button').onclick =  function func(){
 	
-	 var x = document.getElementById("dontclass");
+	 var x = document.querySelector('.section6');
 	
 	x.querySelector("p:not(.intro)").style.background = "transparent"; 
 	
 }
 
 
-document.getElementById('btn6').onclick = function func(){
-	var x = document.getElementById('oddli');
+document.querySelector('.section7 button').onclick = function func(){
+	var x = document.querySelector('.section7');
 	var y = x.querySelectorAll("li");
 	y[3].style.background ="red";
 	y[4].style.background ="red";
 	y[5].style.background ="red";
 }
 
-document.getElementById('btn7').onclick = function func(){
+document.querySelector('.section8 button').onclick = function func(){
 
- var x = document.getElementById('except1');
+ var x = document.querySelector('.section8');
  var y = x.querySelectorAll("li:not(:first-of-type)");
  var len = y.length;
  var i;
@@ -76,26 +77,26 @@ document.getElementById('btn7').onclick = function func(){
 }
 
 
-document.getElementById('tab1').onclick = function func(){
-	var el = document.getElementById("firsttab");
+document.querySelector('.section4 button:first-of-type').onclick = function func(){
+	var el = document.querySelector('.section4 .content:first-of-type');
 
 // Single class
 	el.classList.add("active");
 
-	var el2 = document.getElementById("secondtab");
+	var el2 = document.querySelector('.section4 .content:nth-of-type(2)');
 	el2.classList.remove("active");
 
 // Multiple calsses
 	//el.classList.add("my-class", "nother-class");
 }
 
-document.getElementById('tab2').onclick = function func(){
-	var el = document.getElementById("secondtab");
+document.querySelector('.section4 button:nth-of-type(2)').onclick = function func(){
+	var el =document.querySelector('.section4 .content:nth-of-type(2)');
 
 // Single class
 	el.classList.add("active");
 
-	var el2 = document.getElementById("firsttab");
+	var el2 = document.querySelector('.section4 .content:first-of-type');
 	el2.classList.remove("active");
 
 
@@ -105,7 +106,7 @@ document.getElementById('tab2').onclick = function func(){
 
 
 
-document.getElementById('btn10').onclick=function func(){
+document.querySelector('.section10 button').onclick=function func(){
 	
 	window.scrollTo(0, 0);
 	
